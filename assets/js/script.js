13c38c433a3f25c6b327747c04af86d6fe4dbd6f
natachadpduarte <natachadpduarte@gmail.com>
2021-03-15 22:43:37 -0300

    LandingPageExercicio12e1503

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+let mapa = document.querySelector("#mapa")
+
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
